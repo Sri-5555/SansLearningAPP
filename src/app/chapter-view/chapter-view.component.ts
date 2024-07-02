1fa7196514a56568429019cdc58a5e0cc72b1d28
sri-5555 <testanalytics@gmail.com>
2024-07-02 22:10:24 +0530

    added loaders and back button for pdf view page

--- src/app/chapter-view/chapter-view.component.ts
+++ src/app/chapter-view/chapter-view.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FileService } from '../services/file.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-chapter-view',
   templateUrl: './chapter-view.component.html',
   styleUrls: ['./chapter-view.component.scss'],
 })
 export class ChapterViewComponent implements OnInit {
-  chapterData:any;
-  chapterContent:string = '';
-  chapterUrl:string='';
-  constructor(private route: ActivatedRoute,private fileService:FileService) { }
+  chapterData: any;
+  chapterContent: string = '';
+  chapterUrl: string = '';
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
-  fileName:any = '';
+  fileName: any = '';
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.chapterData = history.state.chapterData;
@@ -20,23 +20,18 @@ export class ChapterViewComponent implements OnInit {
       this.chapterUrl = `../../assets/studyMaterials/${this.chapterData.url}`;
     });
     this.fileName = '';
-    // this.getPdf();
   }
-
-  // getPdf() {
-  //   this.fileService.fetchPdf(this.pdfSrc).subscribe((res) => {
-  //     console.log('res:',res);
-  //   });
-  // }
 
   ionViewWillEnter() {
     this.route.paramMap.subscribe(params => {
       this.chapterData = history.state.chapterData;
       this.chapterContent = this.chapterData.content;
       this.chapterUrl = `../../assets/studyMaterials/${this.chapterData.url}`;
-      console.log(this.chapterUrl);
     });
     this.fileName = '';
   }
 
+  goBack() {
+    this.router.navigate(['/dashboard/studymaterial']);
+  }
 }
